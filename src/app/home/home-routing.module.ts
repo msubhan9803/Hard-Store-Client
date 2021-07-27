4bf21d1b5b929c0e94a5ce1248f00d497f493773
Ahsan Zulfiqar <mianahsan719@gmail.com>
2021-07-27 11:36:44 +0500

    first commit

---
+++ src/app/home/home-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+
+import { MarijuanaComponent } from "./marijuana/marijuana.component";
+
+const routes: Routes = [
+  {
+    path: "marijuana",
+    component: MarijuanaComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class HomeRoutingModule {}
