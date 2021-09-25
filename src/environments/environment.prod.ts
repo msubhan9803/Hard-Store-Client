10e4ec51921ce97ba912e46cd84e482fc820cec3
msubhansp <mohammadsubhan9803@gmail.com>
2021-09-25 22:32:15 +0500

    Https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   instagram_token: 'INSTA_TOKEN',
   stripe_token: 'STRIPE_TOKEN',
   paypal_token: 'PAYPAL_TOKEN',
-  urlAddress: 'http://uaeslimmers.com/api/',
-  imageAddress: 'http://uaeslimmers.com/uploads/'
+  urlAddress: 'https://uaeslimmers.com/api/',
+  imageAddress: 'https://uaeslimmers.com/uploads/'
 };
