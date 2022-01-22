12d482b637eed3883cc0bae77c1420c03294eadb
msubhan33 <msubhan9803@gmail.com>
2022-01-22 15:44:49 +0500

    faq tweaks

--- src/app/pages/faq/faq.component.ts
+++ src/app/pages/faq/faq.component.ts
@@ -29,32 +29,45 @@ export class FaqComponent implements OnInit {
     document.documentElement.style.setProperty('--theme-deafult', '#5d7227');
     document.documentElement.style.setProperty('--theme-gradient1', '#5d7227');
     document.documentElement.style.setProperty('--theme-gradient2', '#203f15');
-    this.updateFaqList("shipping")
+    // this.updateFaqList("shipping")
+    this.productService.getFaq().subscribe(
+      (res: []) => {
+        this.faqList = res;
+      },
+      err => {
+        Swal.fire({
+          icon: 'error',
+          title: "Something went wrong!",
+          showConfirmButton: false,
+          timer: 1500
+        })
+      }
+    )
 
     this.faqList$.subscribe((res: []) => this.faqList = res)
   }
 
   searchFaq(searchValue: string) {
-    if (searchValue) {
-      if (!searchValue.match(/^[0-9a-z]+$/)) {
-        this.searchValueError = true;
-      } else {
-        this.searchValueError = false;
-        this.searchValue.next(searchValue);
-      }
-      this.showSearchedFaq = true;
-    } else {
-      this.showSearchedFaq = false;
-      this.updateFaqList("shipping")
-    }
+    // if (searchValue) {
+    //   if (!searchValue.match(/^[0-9a-z]+$/)) {
+    //     this.searchValueError = true;
+    //   } else {
+    //     this.searchValueError = false;
+    //     this.searchValue.next(searchValue);
+    //   }
+    //   this.showSearchedFaq = true;
+    // } else {
+    //   this.showSearchedFaq = false;
+    // }
+    this.updateFaqList("shipping", searchValue)
   }
 
-  onTabChange(faqTypq) {
-    this.updateFaqList(faqTypq)
-  }
+  // onTabChange(faqTypq) {
+  //   this.updateFaqList(faqTypq)
+  // }
 
-  updateFaqList(faqType) {
-    this.productService.getFaq().subscribe(
+  updateFaqList(faqType, searchValue) {
+    this.productService.searchFaq(searchValue).subscribe(
       (res: []) => {
         this.faqList = res;
       },
